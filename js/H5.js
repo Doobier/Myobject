1689908cd2d148813e82e63b1928391158dbf187
Mozilla <superrt@163.com>
2016-11-05 23:33:20 +0800

    全局组件开发完毕

---
+++ js/H5.js
@@ -0,0 +1,58 @@
+/**/
+
+var H5 = function(  ){
+	this.id = ( `h5_${Math.random()}` ).replace('.', '_');
+	this.el = $( `<div class="h5" id="${this.id}">` ).hide();
+	this.page = [];
+	$('body').append( this.el );
+
+	this.addPage = function( name, text ){
+		var page = $( `<div class="h5_page section">` );
+
+		if( name != undefined ){
+			page.addClass( `h5_page_${name}`)
+		}
+		if( text != undefined ){
+			page.text( text );
+		}
+		this.el.append(page);
+		this.page.push( page );
+
+		return this;
+	};
+	this.addComponent = function( name, cfg){
+		var cfg = cfg || {};
+		cfg = $.extend({
+			type : 'base'
+		},cfg);
+
+		var component;
+		var page = this.page.slice( -1 )[0];
+
+		switch( cfg.type ){
+			case 'base':
+				component = new H5ComponentBase(name, cfg);
+				break;
+			default:	
+		}
+
+		page.append( component );
+		return this;
+	};
+
+	this.loader = function(){
+		this.el.fullpage({
+			onLeave:function(index, nextIndex, direction){
+				//debugger
+				$(this).find('.h5_component').trigger('onLeave');
+			},
+			afterLoad:function(anchorLink, index){
+				//debugger
+				$(this).find('.h5_component').trigger('onLoad');
+			}
+		});
+		this.page[0].find('.h5_component').trigger('onLoad');
+		this.el.show();
+	}
+	return this;
+}
---
+++ js/H5ComponentBase.js
@@ -0,0 +1,34 @@
+/*基本图文组件对象*/
+
+var H5ComponentBase = function( name, cfg ){
+	var cfg = cfg || {};
+	var id = `h5_c_${Math.random()}`.replace('.','_');
+	//把当前组件类型添加到样式中进行标记
+	var cls = `h5_component_${cfg.type}`;
+	var component = $(`<div class="h5_component ${cls}" h5_component_name_${name} id=${id}>`);
+
+	cfg.text   && component.text( cfg.text );
+	cfg.width  && component.width( cfg.width / 2 );
+	cfg.height && component.height( cfg.height / 2 );
+	cfg.css    && component.css( cfg.css );
+	cfg.bg     && component.css( 'backgroundImage',`url(${cfg.bg})`);
+
+	if( cfg.center === true){
+		component.css({
+			left : '50%',
+			transform : `translateX(-50%)`
+		})
+	}
+
+	component.on('onLoad',function(){
+		component.addClass(`${cls}_load`).removeClass(`${cls}_leave`);
+		cfg.animateIn && component.stop().animate( cfg.animateIn );
+		return false;
+	});
+	component.on('onLeave',function(){
+		component.addClass(`${cls}_leave`).removeClass(`${cls}_load`);
+		cfg.animateOut && component.stop().animate( cfg.animateOut );
+		return false;
+	});
+	return component;
+}
