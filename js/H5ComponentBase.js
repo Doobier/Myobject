33ea18bacba4ad499ae653baed384afa61f16820
Mozilla <superrt@163.com>
2016-11-11 00:26:43 +0800

    face commit

--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -21,13 +21,19 @@ var H5ComponentBase = function( name, cfg ){
 	}
 
 	component.on('onLoad',function(){
-		component.addClass(`${cls}_load`).removeClass(`${cls}_leave`);
-		cfg.animateIn && component.stop().animate( cfg.animateIn );
+		setTimeout(function(){
+			component.addClass(`${cls}_load`).removeClass(`${cls}_leave`);
+			cfg.animateIn && component.stop().animate( cfg.animateIn );
+		},cfg.delay || 0)
+		
 		return false;
 	});
 	component.on('onLeave',function(){
-		component.addClass(`${cls}_leave`).removeClass(`${cls}_load`);
-		cfg.animateOut && component.stop().animate( cfg.animateOut );
+		setTimeout(function(){
+			component.addClass(`${cls}_leave`).removeClass(`${cls}_load`);
+			cfg.animateOut && component.stop().animate( cfg.animateOut );
+		},cfg.delay || 0)
+		
 		return false;
 	});
 	return component;
